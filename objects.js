78a2dafec178f645eed21a3fc54f03dfe3c98fb8
James Stamps Jr <jstampshere@gmail.com>
2020-02-03 16:48:57 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,7 +4,7 @@ var playlist = {artistName: "artistSong"};
 
 function updatePlaylist(playlist, name, song) {
   playlist.Slowdive = "Alison";
-  playlist.My Bloody Valentine = "Sometimes";
+  playlist.MyBloodyValentine = "Sometimes";
     return playlist
 }
 
