2ff787a2f84b66cda351fe1da7c5e84fc765fa00
James Stamps Jr <jstampshere@gmail.com>
2020-02-03 17:40:32 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -8,7 +8,7 @@ function updatePlaylist(playlist, name, song) {
 }
 
 function removeFromPlaylist(playlist, name) {
-  delete.artistName(name) ; 
+  delete artistName.name ; 
     return playlist
     
 }
