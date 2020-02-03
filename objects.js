cb869888c7794668529ea22acf8299808c8993bb
James Stamps Jr <jstampshere@gmail.com>
2020-02-03 17:22:24 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,9 +3,9 @@ var playlist = {artistName: "artistSong"};
 
 
 function updatePlaylist(playlist, name, song) {
-  playlist["name"] = "song" ;
-  playlist["name"] = "song";
-  playlist["name"] = "song";
+  playlist[name] = song ;
+  playlist[name] = song;
+  playlist[name] = song;
         return playlist
 }
 
