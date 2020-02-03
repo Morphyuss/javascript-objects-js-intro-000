4544eead116dd7e1c4a9fd310fc1e4bc6a9d6afe
James Stamps Jr <jstampshere@gmail.com>
2020-02-03 17:24:08 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,8 +4,6 @@ var playlist = {artistName: "artistSong"};
 
 function updatePlaylist(playlist, name, song) {
   playlist[name] = song ;
-  playlist[name] = song;
-  playlist[name] = song;
         return playlist
 }
 
